3b1e739b64679168151a7fa314d5257400f92566
RichetinB <baptisterichetin@gmail.com>
2024-01-16 10:32:27 +0100

    ajout de scene

--- Assets/Script/Enemy/Enemy_deplacement.cs
+++ Assets/Script/Enemy/Enemy_deplacement.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class Enemy_deplacement : MonoBehaviour
 {
-    [SerializeField] GameObject targetDestination;
     [SerializeField] float speed;
 
     Rigidbody2D rgdbd2d;
+    Transform playerTransform;
 
     private void Awake()
     {
         rgdbd2d = GetComponent<Rigidbody2D>();
+        playerTransform = FindObjectOfType<PlayerMove>().transform;
+
+        if (playerTransform == null)
+        {
+            Debug.LogError("Le script PlayerMove n'a pas été trouvé dans la scène ou ne contient pas de Transform.");
+        }
     }
 
     private void FixedUpdate()
     {
-       
-        Vector3 direction = (targetDestination.transform.position - transform.position).normalized;
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
         rgdbd2d.velocity = direction * speed;
     }
 }
